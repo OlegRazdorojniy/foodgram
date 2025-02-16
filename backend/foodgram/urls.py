77e2b2b03109e0d13d42452b437ac7d6a729b406
OlegRazdorojniy <razdorhnyy@mail.ru>
2025-02-16 22:12:37 +0700

    Test1

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,17 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from recipes.views import RecipeViewSet, TagViewSet
+
+router = DefaultRouter()
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include(router.urls)),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
