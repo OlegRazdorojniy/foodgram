41f1c87ffd857a071793c7ae3c8f2bbf7ff3a12b
OlegRazdorojniy <razdorhnyy@mail.ru>
2025-02-17 02:38:56 +0700

    Test1

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -5,7 +5,6 @@ from django.urls import include, path
 from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
 from rest_framework.routers import DefaultRouter
 
-
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
