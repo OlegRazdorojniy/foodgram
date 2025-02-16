3dc30c58e9fc9c63d6023c6ba1393c63d74097cb
OlegRazdorojniy <razdorhnyy@mail.ru>
2025-02-16 17:08:45 +0700

    Test1

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,37 +1,54 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обязателен для создания пользователя")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, username, password, **extra_fields)
 
 
 class User(AbstractUser):
     email = models.EmailField(
         max_length=254,
         unique=True,
-        verbose_name='Электронная почта',
+        verbose_name="Электронная почта",
         blank=False,
         null=False
     )
     first_name = models.CharField(
         max_length=150,
-        verbose_name='Имя',
+        verbose_name="Имя",
     )
     last_name = models.CharField(
         max_length=150,
-        verbose_name='Фамилия',
+        verbose_name="Фамилия",
     )
     avatar = models.ImageField(
-        upload_to='users/avatars/',
+        upload_to="users/avatars/",
         blank=True,
         null=True,
-        verbose_name='Аватар',
+        verbose_name="Аватар",
     )
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
+    objects = UserManager()
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-        ordering = ['id']
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["id"]
 
 
 class Subscription(models.Model):
